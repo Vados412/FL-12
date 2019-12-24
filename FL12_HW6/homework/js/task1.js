aba193fc3a6e7b8600c29373be1c2c7c0765443d
Vadym Gerasimov <58165043+Vados412@users.noreply.github.com>
2019-12-24 19:26:30 +0200

    fix eslint errors

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,20 +1,20 @@
-var a = parseFloat(prompt("Input Number A:", 0));
-var b = parseFloat(prompt("Input Number B:", 0));
-var c = parseFloat(prompt("Input Number C:", 0));
+let a = parseFloat(prompt('Input Number A:', 0));
+let b = parseFloat(prompt('Input Number B:', 0));
+let c = parseFloat(prompt('Input Number C:', 0));
 const two = 2;
 const four = 4;
 const sotka = 100;
-var d = b * b - four * a * c;
-if (isNaN(a) == true || isNaN(b) == true || isNaN(c) == true || a == 0) {
-  alert("Invalid input data");
-} else if (a == 0 && b == 0 && c == 0) {
-  console.log("x=0");
-} else if (a != 0 && d > 0) {
-  let x1 = eval(Math.round(((-b + Math.sqrt(d)) / (two * a)) * sotka) / sotka);
-  let x2 = eval(Math.round(((-b - Math.sqrt(d)) / (two * a)) * sotka) / sotka);
-  console.log("x1=", x1, "and x2=", x2);
-} else if (a != 0 && d == 0) {
-  console.log("x=" + eval(Math.round((-b / two) * a * sotka) / sotka));
+let d = b * b - four * a * c;
+if (isNaN(a) === true || isNaN(b) === true || isNaN(c) === true || a === 0) {
+  alert('Invalid input data');
+} else if (a === 0 && b === 0 && c === 0) {
+  console.log('x=0');
+} else if (a !== 0 && d > 0) {
+  let x1 = Math.round((-b + Math.sqrt(d)) / (two * a) * sotka) / sotka;
+  let x2 = Math.round((-b - Math.sqrt(d)) / (two * a) * sotka) / sotka;
+  console.log('x1=', x1, 'and x2=', x2);
+} else if (a !== 0 && d === 0) {
+  console.log('x=' + Math.round(-b / two * a * sotka) / sotka);
 } else {
-  alert("no solution");
+  alert('no solution');
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,16 +1,16 @@
-var a = parseInt(prompt("Input A:"));
-var b = parseInt(prompt("Input B:"));
-var c = parseInt(prompt("Input C:"));
-if (isNaN(a) == true || isNaN(b) == true || isNaN(c) == true) {
-  alert("input values should be ONLY numbers");
+let a = parseInt(prompt('Input A:'));
+let b = parseInt(prompt('Input B:'));
+let c = parseInt(prompt('Input C:'));
+if (isNaN(a) === true || isNaN(b) === true || isNaN(c) === true) {
+  alert('input values should be ONLY numbers');
 } else if (a <= 0 || b <= 0 || c <= 0) {
-  alert("A triangle must have 3 sides with a positive definite length");
+  alert('A triangle must have 3 sides with a positive definite length');
 } else if (a + b <= c || a + c <= b || b + c <= a) {
-  alert("Triangle doesn’t exist");
-} else if (a != b && a != c && b != c) {
-  console.log("Scalene triangle");
-} else if (a == b && b == c) {
-  console.log("Equilateral triangle");
+  alert('Triangle doesn’t exist');
+} else if (a !== b && a !== c && b !== c) {
+  console.log('Scalene triangle');
+} else if (a === b && b === c) {
+  console.log('Equilateral triangle');
 } else {
-  console.log("Isosceles triangle");
+  console.log('Isosceles triangle');
 }
