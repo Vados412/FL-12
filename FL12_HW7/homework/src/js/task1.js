2ff7d8ed54b47d132fb77a4c7d0f81b6d129b645
Vadym Gerasimov <58165043+Vados412@users.noreply.github.com>
2019-12-29 10:59:53 +0200

    1

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,50 @@
-// Your code goes here
+let loginMail = ["user@gmail.com", "admin@gmail.com"];
+let passwords = ["UserPass", "AdminPass"];
+let loginInput = prompt("Input your E-Mail (xxxxxx@xxxx.xx)");
+
+if (typeof loginInput === "object" || loginInput === "") {
+  alert("Canceled");
+} else if (loginInput.length < 5) {
+  alert("I don't know any emails having name length less than 5 symbols");
+} else if (loginMail.includes(loginInput, 0) === true) {
+  let passwordInput = prompt("Input your Password");
+  if (typeof passwordInput === "object" || passwordInput === "") {
+    alert("Canceled");
+  } else if (
+    (loginInput === loginMail[0] && passwordInput === passwords[0]) ||
+    (loginInput === loginMail[1] && passwordInput === passwords[1])
+  ) {
+    let pwdChange = confirm("Do you want to change your password?");
+    if (pwdChange === true) {
+      let oldpasswordInput = prompt("Input your old Password");
+      if (typeof oldpasswordInput === "object" || oldpasswordInput === "") {
+        alert("Canceled");
+      } else if (
+        (loginInput === loginMail[0] && oldpasswordInput !== passwords[0]) ||
+        (loginInput === loginMail[1] && oldpasswordInput !== passwords[1])
+      ) {
+        alert("Wrong password");
+      } else {
+        let newpasswordInput = prompt("Input your new Password");
+        if (typeof newpasswordInput === "object" || newpasswordInput === "") {
+          alert("Canceled");
+        } else if (newpasswordInput.length < 6) {
+          alert("It’s too short password. Sorry.");
+        } else {
+          let reenternewpassword = prompt("Input your new Password again");
+          if (reenternewpassword !== newpasswordInput) {
+            alert("You wrote the wrong password.");
+          } else {
+            alert("You have successfully changed your password.");
+          }
+        }
+      }
+    } else {
+      alert("You have failed the change.");
+    }
+  } else {
+    alert("Wrong password");
+  }
+} else {
+  alert("I don’t know you");
+}
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,48 @@
-// Your code goes here
+let max = 8;
+let min = 0;
+let atempts = 3;
+let startGame = confirm("Do you want to play a game?");
+let randomNumber = parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
+let prize=0;
+let totalPrize=0;
+let sotka=100;
+let poltos=50;
+let popoltosa=25;
+console.log(randomNumber);
+if (startGame !== true) {
+  alert("You did not become a billionaire, but can.");
+} else {
+  game:
+    for (;atempts > 0;) {
+    if (atempts===3) {
+        let userNumber = parseInt(prompt("Choose a roulette pocket number from 0 to 8"));
+        if (userNumber===randomNumber) {
+            totalPrize=totalPrize+sotka;
+            let continueGame = confirm('Congratulation, you won!   Your prize is: '+totalPrize+ '$. Do you want to continue?');
+        } else {
+            atempts--;
+            alert("Attempts left: " + atempts);
+        }
+    } /*else if (atempts===2) {
+        let newrandomNumber = parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
+        let userNumber = parseInt(prompt("Choose a roulette pocket number from 0 to 8"));
+        if (userNumber===randomNumber) {
+            totalPrize=totalPrize+poltos;
+        } else {
+            atempts--;
+            alert("Attempts left: " + atempts);
+        }
+    } else if (atempts===1) {
+        let newrandomNumber = parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
+        let userNumber = parseInt(prompt("Choose a roulette pocket number from 0 to 8"));
+        if (userNumber===newrandomNumber) {
+            totalPrize=totalPrize+popoltosa;
+        } else {
+            atempts--;
+            alert("Attempts left: " + atempts);
+            let repeatGame = confirm('Do you want to play a new game?');
+        } */
+
+    
+  }
+}
